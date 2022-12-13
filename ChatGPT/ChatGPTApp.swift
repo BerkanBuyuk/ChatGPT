dc5e193348486fc6d72347dd22e94e1137e623c4
Berkan Büyük <berkanbyk55@gmail.com>
2022-12-13 16:18:20 +0300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Berkan on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
